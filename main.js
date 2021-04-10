8394796dfc3e843dcb86568f4c1fc0a2813bd898
Gaurang-1 <72252176+Gaurang-1@users.noreply.github.com>
2021-04-10 16:13:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 img="";
+status="";
 
 function preload(){
     img=loadImage('dog_cat.jpg');
@@ -7,6 +8,21 @@ function preload(){
 function setup(){
     canvas=createCanvas(640,420);
     canvas.center();
+    objectDetector=ml5.objectDetector("cocossd",modalLoaded);
+    document.getElementById("status").innerHTML="Status = Detecting objects";
+}
+
+function modalLoaded(){
+    console.log("Modal Loaded");
+    status=true;
+    objectDetector.detect(img,gotResults);
+}
+
+function gotResults(error,results){
+    if (error){
+        console.log(error);
+    }
+    console.log(results);
 }
 
 function draw(){
@@ -16,4 +32,10 @@ function draw(){
     noFill();
     stroke('#FF0000');
     rect(30,60,450,350);
+
+    fill('#FF0000');
+    text("cat",320,120);
+    noFill();
+    stroke('#FF0000');
+    rect(300,90,270,320);
 }
